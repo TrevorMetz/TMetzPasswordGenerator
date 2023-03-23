7d818c152d6baea61a262e997882a2008130195e
Trevor Metz <trevorgeorgemetz@gmail.com>
2023-03-23 18:05:41 -0500

    add length input & validation

--- Develop/script.js
+++ Develop/script.js
@@ -5,11 +5,18 @@ function getRandomInt(max) {
 }
 
 function generatePassword() {
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?';
   var charactersLength = characters.length;
+  var passwordLength = prompt("How long do you want the password? (Choose a number from 8 to 128)");
+  var passwordLengthIsBad = passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)
+  
+  while (passwordLengthIsBad) {
+    passwordLength = prompt("Please enter number from 8 to 128")
+    passwordLengthIsBad = passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)
+  }
 
   var randomPassword = ""
-  for (let index = 0; index < 8; index++) {
+  for (let index = 0; index < passwordLength; index++) {
     var randomCharacterPosition = getRandomInt(charactersLength)
 
     var randomCharacter = characters.charAt(randomCharacterPosition)
@@ -32,19 +39,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
